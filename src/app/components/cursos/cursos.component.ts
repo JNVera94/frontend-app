d88f25de893167c3a8e7bc9b07a91d132b42531a
Raúl Bruno <brunoraulok@gmail.com>
2023-11-17 18:54:28 -0300

    pag funcionamiento modificado

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -8,5 +8,21 @@ import { RouterLink } from '@angular/router';
  
 })
 export class CursosComponent {
+  cursos=[
+  { nombre: 'Desarrollo Web', categoria: 'principiante',
+   descripcion:'Aprende HMTL, CSS y JavaScript para Construir el Futuro Exploraremos potentes frameworks como Angular y Node.js Cada línea de código será una nueva experiencia sorprendente', 
+   icono:'.src/assets/motherboard.svg'},
+  { nombre: 'Base de DatoOS', categoria: 'principiante',descripcion:'probandoooo22' },
+  { nombre: 'Diseño de Sistemas', categoria: 'avanzado',descripcion:'probandoooo333' },
+];
 
+categoriaSeleccionada = 'all';
+
+  filtrarPorCategoria(categoria: string) {
+    this.categoriaSeleccionada = categoria;
+  }
+
+  mostrarTodos() {
+    this.categoriaSeleccionada = 'all';
+  }
 }
